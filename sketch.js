41eb4ac3988d95dcc6b6117ccde57caca0263e80
TheCoder215 <71939694+TheCoder215@users.noreply.github.com>
2020-11-28 12:26:19 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var paper;
+var tc1, tc2, tc3;
+var ground
+
+function setup() {
+	createCanvas(800, 400);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ground = new Ground(400,365,800,15);
+	paper = new Paper(100,342.5,18);
+	tc1 = new Trashcan(510,298,15,120);
+	tc2 = new Trashcan(650,298,15,120);
+	tc3 = new Trashcan(580,343,140,30)
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {	
+  rectMode(CENTER);
+  background("lightblue");
+  
+  ground.display();
+  paper.display();
+  tc1.display();
+  tc2.display();
+  tc3.display();
+
+}
+
+function keyPressed(){
+
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(paper.body, paper.body.position, {x: 30, y: -50})
+	}
+}
+
+
+
